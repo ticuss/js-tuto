e2a6a18a7bf53127ab9c1928b96fa3dda19c23f2
ticuss <ticu369@gmail.com>
2020-06-15 22:23:46 +0200

    lesson 4

---
+++ 4-Lesson/script.js
@@ -0,0 +1,118 @@
+// Function constructor 
+/*
+
+var john = {
+    name:'John',
+    yearOfBirth: 1990, 
+    job : 'teacher'
+};
+//function constructor uppercase 
+var Person = function(name , yearOfBirth , job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth; 
+    this.job = job;
+    }
+
+
+
+ Person.prototype.calculateAge =
+  function() {
+    console.log(2020 - this.yearOfBirth);
+};
+Person.prototype.lastName = 'Smith' ;
+
+// instance
+var john = new Person('John' , 1990 , 'teacher'); 
+var jane = new Person('Jane' , 1969 , 'designer');
+var mark = new Person('Mark' , 1948 , 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+console.log(mark.lastName);*/
+
+
+
+///object.create 
+/*
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+
+john.name = 'John ' ; 
+john.yearOfBirth = '1990';
+john.job = 'teacher' ;
+
+var jane = Object.create(personProto , {
+    name : {value : 'Jane'},
+    yearOfBirth  : {value : 1969},
+    job : {value : 'designer'}
+   
+});
+
+*/
+
+
+//primitives vs object
+
+var a = 23 ; 
+var b = a; 
+
+a = 46 ;
+console.log(a);
+
+console.log(b);
+
+
+//Objects
+var obj1 = {
+    name: 'John',
+    age: 26 
+};
+
+var obj2 = obj1;
+obj1.age = 30 ; 
+
+
+
+console.log(obj1.age);
+
+console.log(obj2.age);
+
+
+
+//functions 
+
+var age = 27 ;
+var obj = {
+    name : 'Jonas',
+    city : 'Lisbon'
+};
+
+function change (a,b) {
+    a= 30 ;
+    b.city = 'San Francisco';
+}
+
+change (age,obj) ; 
+
+console.log(obj.city);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
